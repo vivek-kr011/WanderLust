37d75ebd1cbab37aee64084f6968d7f1df2852b9
vivekkr620 <gk6629086@gmail.com>
2026-08-24 21:56:04 +0530

    style: formatting ExpressError code

--- backend/utils/ExpressError.js
+++ backend/utils/ExpressError.js
@@ -1,13 +1,3 @@
-// class ExpressError extends Error {
-//     constructor(statusCode, message) {
-//         super();
-//         this.statusCode = statusCode;
-//         this.message = message;
-//     }
-// }
-// module.exports = ExpressError;
-
-
 /* ========================================== */
 class ExpressError extends Error {
   constructor(statusCode, message) {
